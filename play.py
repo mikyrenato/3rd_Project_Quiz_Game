cbecea3e358405925cf831a55e2f2a6e574a5b68
mikyrenato <mikyrenato@yahoo.com>
2022-08-19 19:11:30 +0000

    Working on creating leadrboard function

--- play.py
+++ play.py
@@ -12,8 +12,51 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('quiz_game')
 
-quiz = SHEET.worksheet('Sheet1')
+def get_quiz_data():
 
-data = quiz.get_all_values()
+    while True:
+        print("Please enter quiz data.")
 
-print(data)
+        data_str = input("Enter your data here:\n")
+
+        quiz_data = data_str.split(",")
+
+        if validate_data(quiz_data):
+            print("Data is valid!")
+            break
+
+    return quiz_data
+
+
+def validate_data(values):
+
+    try:
+        [int(value) for value in values]
+        if len(values) != 2:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
+
+
+def update_worksheet(data, worksheet):
+
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
+
+
+def main():
+
+    data = get_quiz_data()
+    quiz_data = [int(num) for num in data]
+    update_worksheet(quiz_data, "Sheet1")
+
+
+print("Welcome to the football quiz")
+main()
