69ed2f3f74a0719714413dc351d1a6432e12beb7
mikyrenato <mikyrenato@yahoo.com>
2022-08-19 17:33:53 +0000

    Wire up APIs and confirm access to google sheets

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
+
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('quiz_game')
+
+
 nickname = ""
 nickname = input("Please enter your nickname: \n")
 print("Hello "+str(nickname)+", below is the first question, good luck!")
