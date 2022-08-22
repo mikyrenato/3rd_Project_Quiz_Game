d077a201252140be6723e552fffe8652b9f92124
mikyrenato <104921651+mikyrenato@users.noreply.github.com>
2022-08-22 11:21:15 +0100

    Update run.py
    Fix errors in PEP8

--- run.py
+++ run.py
@@ -2,9 +2,6 @@ import gspread
 from google.oauth2.service_account import Credentials
 from tabulate import tabulate
 
-#pip3 install gspread google-auth
-#pip3 freeze > requirements.txt
-#pip3 install tabulate
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -43,7 +40,7 @@ def start_quiz():
             selection = input("Enter (A, B, C, or D): \n")
             selection = selection.upper()
             if selection not in ('A', 'B', 'C', 'D'):
-                print("Invalid choice, the only options are: A, B, C, or D")
+                print("Invalid choice, the only options are A, B, C, or D")
             else:
                 break
 
@@ -52,7 +49,8 @@ def start_quiz():
         correct_selections += check_result(questions.get(key), selection)
         question_num += 1
 
-    resp = input("Would you like to reveal the answers together with your result % ? (yes or no): \n")
+    resp = input("Would you like to reveal the answers together"
+                 "with your result % ? (yes or no): \n")
     resp = resp.upper()
 
     if resp == "YES":
@@ -81,7 +79,6 @@ def check_result(result, selection):
     if selection == result:
         print("YOU ARE CORRECT!")
         return 1
-        
     else:
         print("YOU ARE WRONG!")
         return 0
@@ -117,7 +114,7 @@ def restart_game():
     if response == "YES":
         return True
     else:
-        return False 
+        return False
 
 
 # -------------------------
@@ -140,11 +137,11 @@ def leaderboard():
 
     data = lead.get_all_values()
 
-    size = lambda dat: float(dat[1])
+    size = lambda dat: dat[1]
 
     data.sort(key=size, reverse=True)
 
-    print(tabulate(data[0:10], headers=['Nickname', 'Score']))
+    print(tabulate(data[0:11], headers=['Leaderboard', 'Table']))
 
 
 # -------------------------
@@ -152,28 +149,45 @@ def leaderboard():
 
 questions = {
  "1: Which player scored the fastest hat-trick in the Premier League?": "A",
- "2: Which player, with 653 games, has made the most Premier League appearances?": "B",
- "3: Three players share the record for most Premier League red cards (8). Who are they?": "C",
+ "2: Which player, with 653 games, has made the most"
+ "Premier League appearances?": "B",
+ "3: Three players share the record for most Premier"
+ "League red cards (8). Who are they?": "C",
  "4: With 260 goals, who is the Premier League's all-time top scorer?": "D",
  "5: When was the inaugural Premier League season?": "A",
  "6: Which team won the first Premier League title?": "B",
- "7: With 202 clean sheets, which goalkeeper has the best record in the Premier League?": "C",
+ "7: With 202 clean sheets, which goalkeeper has the"
+ "best record in the Premier League?": "C",
  "8: How many clubs competed in the inaugural Premier League season?": "D",
- "9: Which three players shared the Premier League Golden Boot in 2018-19?": "A",
- "10: The fastest goal scored in Premier League history came in 7.69 seconds. Who scored it?": "B"
+ "9: Which three players shared the"
+ "Premier League Golden Boot in 2018-19?": "A",
+ "10: The fastest goal scored in Premier League"
+ "history came in 7.69 seconds. Who scored it?": "B"
 }
 
 
-options = [["A. Sadio Mane", "B. Cristiano Ronaldo", "C. Michael Owen", "D. Didier Drogba"],
-          ["A. Wayne Rooney", "B. Gareth Barry", "C. Rio Ferdinand", "D. John Terry"],
-          ["A. Nemanja Vidic, John Terry and Patrice Evra", "B. Patrick Vieira, Virgil Van Dijk and Duncan Ferguson", "C. Patrick Vieira, Richard Dunne and Duncan Ferguson", "D. Nemanja Vidic, John Terry and Richard Dunne"],
-          ["A. Cristiano Ronaldo", "B. Carlos Tevez", "C. Thierry Henry", "D. Alan Shearer"],
-          ["A. 1992-93", "B. 1991-1992", "C. 1989-90", "D. 1993-94"],
-          ["A. Chelsea", "B. Manchester United", "C. Arsenal", "D. Liverpool"],
-          ["A. Edwin Van der Sar", "B. David De Gea", "C. Petr Cech", "D. Peter Schmeichel"],
-          ["A. 26", "B. 24", "C. 20", "D. 22"],
-          ["A. Pierre-Emerick Aubameyang, Mohamed Salah and Sadio Mane", "B. Harry Kane, Mohamed Salah and Sadio Mane", "C. Pierre-Emerick Aubameyang, Hiung Ming Son and Sadio Mane. ", "D. Pierre-Emerick Aubameyang, Mohamed Salah and Jamie Vardy"],
-          ["A. Cristiano Ronaldo", "B. Shane Long", "C. Carlos Tevez", "D. Zlatan Ibrahimovic"]]
+options = [["A. Sadio Mane", "B. Cristiano Ronaldo",
+            "C. Michael Owen", "D. Didier Drogba"],
+           ["A. Wayne Rooney", "B. Gareth Barry",
+            "C. Rio Ferdinand", "D. John Terry"],
+           ["A. Nemanja Vidic, John Terry and Patrice Evra",
+           "B. Patrick Vieira, Virgil Van Dijk and Duncan Ferguson",
+            "C. Patrick Vieira, Richard Dunne and Duncan Ferguson",
+            "D. Nemanja Vidic, John Terry and Richard Dunne"],
+           ["A. Cristiano Ronaldo", "B. Carlos Tevez",
+           "C. Thierry Henry", "D. Alan Shearer"],
+           ["A. 1992-93", "B. 1991-1992", "C. 1989-90", "D. 1993-94"],
+           ["A. Chelsea", "B. Manchester United",
+           "C. Arsenal", "D. Liverpool"],
+           ["A. Edwin Van der Sar", "B. David De Gea",
+           "C. Petr Cech", "D. Peter Schmeichel"],
+           ["A. 26", "B. 24", "C. 20", "D. 22"],
+           ["A. Pierre-Emerick Aubameyang, Mohamed Salah and Sadio Mane",
+           "B. Harry Kane, Mohamed Salah and Sadio Mane",
+            "C. Pierre-Emerick Aubameyang, Hiung Ming Son and Sadio Mane. ",
+            "D. Pierre-Emerick Aubameyang, Mohamed Salah and Jamie Vardy"],
+           ["A. Cristiano Ronaldo", "B. Shane Long",
+            "C. Carlos Tevez", "D. Zlatan Ibrahimovic"]]
 
 
 start_quiz()
