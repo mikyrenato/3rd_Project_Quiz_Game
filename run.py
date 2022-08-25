4477b2f98fc706803cd722ed86a6b90978f503bc
mikyrenato <mikyrenato@yahoo.com>
2022-08-25 10:14:26 +0000

    Fixing bugs

--- run.py
+++ run.py
@@ -205,34 +205,3 @@ print("Thank you and have a nice day! :)")
 
 
 # -------------------------
-
-from tkinter import *
-
-ws = Tk()
-ws.title('PythonGuides')
-ws.geometry('500x300')
-ws.config(bg='yellow')
-
-img = PhotoImage(file="j.jpg")
-label = Label(
-    ws,
-    image=img
-)
-label.place(x=0, y=0)
-
-text = Text(
-    ws,
-    height=10,
-    width=53
-)
-text.place(x=30, y=50)
-
-button = Button(
-    ws,
-    text='SEND',
-    relief=RAISED,
-    font=('Arial Bold', 18)
-)
-button.place(x=190, y=250)
-
-ws.mainloop()
