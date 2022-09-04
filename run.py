928d47b531ccf2ecd56486579b0e58ce2bed1114
mikyrenato <104921651+mikyrenato@users.noreply.github.com>
2022-09-04 17:19:00 +0100

    Update run.py
    Fixing bugs

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-#pip3 install gspread google-auth
-#pip3 freeze > requirements.txt
-#pip3 install tabulate
+# pip3 install gspread google-auth
+# pip3 freeze > requirements.txt
+# pip3 install tabulate
 
 import gspread
 from google.oauth2.service_account import Credentials
@@ -21,8 +21,6 @@ SHEET = GSPREAD_CLIENT.open('quiz_game')
 
 
 print("Do you know much about football? Can you make it on the leaderboard?")
-print("You will have 20 questions about premier league and world cup.")
-print("Each question will give you 5 points out of 100.")
 nickname = ""
 nickname = input("Please enter your nickname: \n")
 print("Hello "+str(nickname)+", below is the first question, good luck!")
@@ -30,7 +28,7 @@ print("Hello "+str(nickname)+", below is the first question, good luck!")
 
 # -------------------------
 
-#This is the main function which will run the quiz
+# This is the main function which will run the quiz
 def start_quiz():
 
     selections = []
@@ -42,7 +40,7 @@ def start_quiz():
         print(key)
         for i in options[question_num-1]:
             print(i)
-        #This is the validation in case the user is choosing an invalid answer
+        # This is the validation in case the user is choosing an invalid answer
         while True:
             selection = input("Enter (A, B, C, or D): \n")
             selection = selection.upper()
@@ -55,7 +53,7 @@ def start_quiz():
 
         correct_selections += check_result(questions.get(key), selection)
         question_num += 1
-    #the user is being asked if he want to see the correct answers
+    # the user is being asked if he want to see the correct answers
     resp = input("Would you like to reveal the answers together"
                  "with your result % ? (yes or no): \n")
     resp = resp.upper()
@@ -80,7 +78,7 @@ def start_quiz():
 
 # -------------------------
 
-#This function is telling the user if the answer was correct/incorrect
+# This function is telling the user if the answer was correct/incorrect
 def check_result(result, selection):
 
     if selection == result:
@@ -93,7 +91,7 @@ def check_result(result, selection):
 
 # -------------------------
 
-#This fucntion will show the correct results and the choices
+# This fucntion will show the correct results and the choices
 def show_score(correct_selections, selections):
     print("-------------------------")
     print("RESULTS")
@@ -112,7 +110,7 @@ def show_score(correct_selections, selections):
 
 # -------------------------
 
-#This fucntion is asking the user if he wants to try again or end the game
+# This fucntion is asking the user if he wants to try again or end the game
 def restart_game():
 
     response = input("Would you like to try again? (yes or no): \n")
@@ -126,7 +124,7 @@ def restart_game():
 
 # -------------------------
 
-#This fucntion is updating the leaderboard worksheet
+# This fucntion is updating the leaderboard worksheet
 def update_worksheet(data, worksheet):
 
     print(f"Updating {worksheet} worksheet...\n")
@@ -137,7 +135,7 @@ def update_worksheet(data, worksheet):
 
 # -------------------------
 
-#This function is sorting from highest to lowest the top 10 results from the worksheet
+# Sorting from highest to lowest the top 10 results from the worksheet
 def leaderboard():
 
     lead = SHEET.worksheet('Leaderboard')
@@ -153,7 +151,7 @@ def leaderboard():
 
 # -------------------------
 
-#these are the quiz questions and answers
+# These are the quiz questions and answers
 questions = {
  "1: Which player scored the fastest hat-trick in the Premier League?": "A",
  "2: Which player, with 653 games, has made the most"
@@ -169,20 +167,26 @@ questions = {
  "9: Which three players shared the"
  "Premier League Golden Boot in 2018-19?": "A",
  "10: The fastest goal scored in Premier League"
- "history came in 7.69 seconds. Who scored it?": "B"
- "11: There have been two World Cup trophies. What was the name of the first?": "C"
- "12: Which country won the first ever World Cup in 1930?": "D"
- "13: Which country has won the most World Cups?": "A"
- "14: Three countries have won the World Cup twice. Can you name them?": "B"
- "15: Which country has appeared in three World Cup finals, but never won the competition?": "C"
- "16: The 2026 World Cup will be hosted across three different countries. Can you name them?": "D"
- "17: In which World Cup did Diego Maradona score his infamous 'Hand of God' goal?": "A"
- "18: The record number of World Cup goals is 16, scored by who?": "B"
- "19: Three people have won the World Cup as a player and as a coach. Mario Zagallo, Didier Deschamps and... can you name the third?": "C"
- "20: Two English players have won the World Cup Golden Boot. Who are they?": "D"
+ "history came in 7.69 seconds. Who scored it?": "B",
+ "11: There have been two World Cup trophies."
+ "What was the name of the first?": "C",
+ "12: Which country won the first ever World Cup in 1930?": "D",
+ "13: Which country has won the most World Cups?": "A",
+ "14: Three countries have won the World Cup twice. Can you name them?": "B",
+ "15: Which country has appeared in three World Cup finals,"
+ "but never won the competition?": "C",
+ "16: The 2026 World Cup will be hosted across three different countries."
+ "Can you name them?": "D",
+ "17: In which World Cup did Diego Maradona"
+ "score his infamous 'Hand of God' goal?": "A",
+ "18: The record number of World Cup goals is 16, scored by who?": "B",
+ "19: Three people have won the World Cup as a player and as a coach."
+ "Mario Zagallo, Didier Deschamps and... can you name the third?": "C",
+ "20: Two English players have won the World Cup Golden Boot."
+ "Who are they?": "D"
 }
 
-#These are the quiz options
+# These are the quiz options
 options = [["A. Sadio Mane", "B. Cristiano Ronaldo",
             "C. Michael Owen", "D. Didier Drogba"],
            ["A. Wayne Rooney", "B. Gareth Barry",
@@ -205,20 +209,29 @@ options = [["A. Sadio Mane", "B. Cristiano Ronaldo",
             "D. Pierre-Emerick Aubameyang, Mohamed Salah and Jamie Vardy"],
            ["A. Cristiano Ronaldo", "B. Shane Long",
             "C. Carlos Tevez", "D. Zlatan Ibrahimovic"],
-            ["A. World Football Cup / The Cup", "B. World Cup Trophy / World Trophy",
-             "C. Jules Rimet Trophy / Victory", "D. Victory / orld Football Cup"],
-            ["A. Argentina", "B. Brazil", "C. Spain", "D. Uruguay"],
-            ["A. Brazil", "B. Italy", "C. Germany", "D. Mexico"],
-            ["A. Spain, Italy and Germany", "B. Argentina, France and Uruguay",
-             "C. Argentina, Brazil and Spain", "D. Italy, Netherlands and Uruguay"],
-            ["A. Italy", "B. Germany", "C. Netherlands", "D. Spain"],
-            ["A. Spain, Italy and Germany", "B. Argentina, France and Uruguay",
-             "C. Italy, Netherlands and Uruguay", "D. United States, Canada and Mexico"],
-            ["A. Mexico 1986", "B. Germany 1990", "C. Spain 1982", "D. Italy 1986"],
-            ["A. Cristiano Ronaldo", "B. Miroslav Klose", "C Lionel Messi", "D. Fernando Torres"],
-            ["A. Diego Maradona", "B. Gica Hagi", "C. Franz Beckenbauer", "D. Ronaldo Nazario"],
-            ["A. Jamie Vardy (2014) and Harry Kane (2018)", "B. Gary Lineker (1986) and Jamie Vardy (2018)",
-             "C. Gary Lineker (1986) and Raheem Sterling (2018)", "D. Gary Lineker (1986) and Harry Kane (2018)"]]
+           ["A. World Football Cup / The Cup",
+           "B. World Cup Trophy / World Trophy",
+            "C. Jules Rimet Trophy / Victory",
+            "D. Victory / orld Football Cup"],
+           ["A. Argentina", "B. Brazil", "C. Spain", "D. Uruguay"],
+           ["A. Brazil", "B. Italy", "C. Germany", "D. Mexico"],
+           ["A. Spain, Italy and Germany", "B. Argentina, France and Uruguay",
+            "C. Argentina, Brazil and Spain",
+            "D. Italy, Netherlands and Uruguay"],
+           ["A. Italy", "B. Germany", "C. Netherlands", "D. Spain"],
+           ["A. Spain, Italy and Germany", "B. Argentina, France and Uruguay",
+            "C. Italy, Netherlands and Uruguay",
+            "D. United States, Canada and Mexico"],
+           ["A. Mexico 1986", "B. Germany 1990",
+            "C. Spain 1982", "D. Italy 1986"],
+           ["A. Cristiano Ronaldo", "B. Miroslav Klose",
+            "C Lionel Messi", "D. Fernando Torres"],
+           ["A. Diego Maradona", "B. Gica Hagi",
+            "C. Franz Beckenbauer", "D. Ronaldo Nazario"],
+           ["A. Jamie Vardy (2014) and Harry Kane (2018)",
+            "B. Gary Lineker (1986) and Jamie Vardy (2018)",
+            "C. Gary Lineker (1986) and Raheem Sterling (2018)",
+            "D. Gary Lineker (1986) and Harry Kane (2018)"]]
 
 
 start_quiz()
@@ -229,4 +242,3 @@ while restart_game():
 
 
 print("Thank you and have a nice day! :)")
-
